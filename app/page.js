901e5b1acd85d8f8b809aaea8593619274cc97a2
Deborah <ayekudeborah18@gmail.com>
2024-08-14 23:58:54 +0100

    paw

--- app/page.js
+++ app/page.js
@@ -1,31 +1,47 @@
 import Image from "next/image";
 import getStripe from "@/utils/get-stripe";
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { AppBar, Button, Container, Toolbar, Typography } from "@mui/material";
+import { AppBar, Box, Button, Container, Toolbar, Typography } from "@mui/material";
 import Head from "next/head";
 
 export default function Home() {
   return(
-    <Container maxWidth ="lg">
-      <Head>
-        <tittle>Paw Cards</tittle>
-        <meta name ="description"content="Create flashcard from your text" />
-      </Head>
+      <Container maxWidth ="100vw">
+        <Head>
+          <tittle>Paw Cards</tittle>
+          <meta name ="description"content="Create flashcard from your text" />
+        </Head>
 
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" style={{flexGrow: 1}}>
-            Paw Cards
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" style={{flexGrow: 1}}>
+              Paw Cards
+            </Typography>
+            <SignedOut>
+              <Button color="inherit" href="/sign-in">Login</Button>
+              <Button color="inherit" href="/sign-up">Sign Up</Button>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </Toolbar>
+        </AppBar>
+
+        <Box sx={{textAlign: 'center', my: 4}}>
+          <Typography variant="h2" component="h1" gutterBottom>
+            Welcome to Paw Cards🐶
           </Typography>
-          <SignedOut>
-            <Button color="inherit" href="/sign-in">Login</Button>
-            <Button color="inherit" href="/sign-up">Sign Up</Button>
-          </SignedOut>
-          <SignedIn>
-            <UserButton />
-          </SignedIn>
-        </Toolbar>
-    </AppBar>
-    </Container>
+          <Typography variant="h5" component="h2" gutterBottom>
+            The easiest way to create flashcards from your text.
+          </Typography>
+          <Button variant="contained" color="primary" sx={{mt: 2, mr: 2}} href="/generate">
+            Get Started
+          </Button>
+          <Button variant="outlined" color="primary" sx={{mt: 2}}>
+            Learn More
+          </Button>
+        </Box>
+      </Container>
+    
     );
 }
