6c5ff8002a322dc4d9df9b944ccb80dc6e0e7cdc
Nafisa-Bashar-Anmul <anmul22n@mtholyoke.edu>
2024-08-20 22:21:56 -0400

    adding updated UI

--- app/generate/page.js
+++ app/generate/page.js
@@ -16,12 +16,7 @@ import {
 } from "@mui/material";
 import { useUser } from "@clerk/nextjs";
 import { useRouter } from "next/navigation";
-import {
-  collection,
-  doc,
-  getDoc,
-  setDoc,
-} from "firebase/firestore";
+import { collection, writeBatch, doc, getDoc } from "firebase/firestore";
 import { db } from "@/firebase"; // Adjust the import based on your file structure
 import ReactFlipCard from "reactjs-flip-card";
 import FlipCard from "@/components/flipcard";
@@ -55,113 +50,60 @@ export default function Generate() {
 
       const data = await response.json();
       setFlashcards(data);
-      console.log(flashcards)
     } catch (error) {
       console.error("Error generating flashcards:", error);
       alert("An error occurred while generating flashcards. Please try again.");
     }
   };
 
-  const userCheck = async () => {
-    // Check if the user-id document exists
-    const docRef = doc(db, "flashcards", `${user.id}`);
-    let userDocRef;
-
-    try {
-      userDocRef = await getDoc(docRef);
-      if (!userDocRef.exists()) {
-        // Create user-id document in Firestore
-        await setDoc(docRef, {
-          email: user.emailAddresses[0].emailAddress || "noemail@example.com", // Example field
-        });
-
-        // Fetch the newly created document to return it
-        userDocRef = await getDoc(docRef);
-      }
-    } catch (error) {
-      console.error(
-        "Error with checking user existence. Maybe you don't exist. Check and try again later.",
-        error
-      );
-      return null;
-    }
-
-    return userDocRef;
-  };
-
-  const flashcardCollectionCheck = async () => {
-    const flashcardSetsCollectionRef = collection(
-      db,
-      `flashcards/${user.id}/flashcardSets`
-    );
-    const flashcardSetId = name; // Replace with the actual flashcardSetId you want to check or create
-    const flashcardSetDocRef = doc(flashcardSetsCollectionRef, flashcardSetId);
-    const flashcardSetDoc = await getDoc(flashcardSetDocRef);
-  
-    // Check if the flashcardSet exists
-    if (!flashcardSetDoc.exists()) {
-      // If flashcardSet does not exist, create it
-      await setDoc(flashcardSetDocRef, {
-        prompt: text, // Example field, replace with your actual prompt
-        createdAt: new Date().toISOString(),
-      });
-  
-      console.log(
-        `flashcardSets subcollection was empty or flashcardSet with ID ${name} did not exist. Created it.`
-      );
-    } else {
-      console.log(`flashcardSet with ID ${name} already exists.`);
-    }
-  
-    // Add flashcards to the flashcards collection with custom IDs
-    const flashcardsCollectionRef = collection(
-      flashcardSetDocRef,
-      "flashcards"
-    );
-  
-    for (let i = 0; i < flashcards.length; i++) {
-      const flashcard = flashcards[i];
-      const flashcardId = `flashcard${i + 1}`; // Custom flashcard ID like flashcard1, flashcard2, etc.
-  
-      const flashcardDocRef = doc(flashcardsCollectionRef, flashcardId);
-      await setDoc(flashcardDocRef, {
-        question: flashcard.front,
-        answer: flashcard.back,
-        createdAt: new Date().toISOString(),
-      });
-    }
-  
-    console.log(`Added ${flashcards.length} flashcards to flashcardSet with ID ${name}.`);
-  };
-
   const saveFlashcards = async () => {
-    console.log(user.id);
-    console.log(user.emailAddresses[0].emailAddress);
-
-    const docRef = doc(db, "flashcards", user.id);
-
-    const userDocRef = await userCheck();
-    if (userDocRef != null) {
-      // await flashcardCollectionCheck(userDocRef);
-      console.log("User either exists or was added succesfully.");
-      flashcardCollectionCheck()
-    } else {
-      console.log("There was an error retreiving or creating user.");
+    if (!name) {
+      alert("Please enter a name for your flashcards collection.");
+      return;
     }
 
+    const batch = writeBatch(db);
+    const userDocRef = doc(collection(db, "users"), user.id);
+    const docSnap = await getDoc(userDocRef);
+
+    if (docSnap.exists()) {
+      const collections = docSnap.data().flashcards || [];
+      if (collections.find((f) => f.name === name)) {
+        alert("Flashcard collection with the same name already exists.");
+        return;
+      } else {
+        collections.push({ name });
+        batch.set(userDocRef, { flashcards: collections }, { merge: true });
+      }
+    } else {
+      batch.set(userDocRef, { flashcards: [{ name }] });
+    }
+
+    const colRef = collection(userDocRef, name);
+    flashcards.forEach((flashcard) => {
+      const cardDocRef = doc(colRef);
+      batch.set(cardDocRef, flashcard);
+    });
+
+    await batch.commit();
     handleCloseDialog();
-    // router.push("/flashcards");
+    router.push("/flashcards");
   };
 
   return (
-    <Box sx={{ backgroundColor: "#FF9913", minHeight: "100vh", display:"flex", 
-      flexDirection: "column",
+    <Box 
+      sx={{
+        backgroundColor: "#FF9913",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
         padding: "20px"
-     }}>
-      <Container maxWidth="md">
+      }}
+    >
+      <Container maxWidth="md" sx={{ zIndex: 1 }}>
         <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Generate  Pawcards
+          <Typography variant="h4" component="h1" sx={{fontFamily:"impact"}} gutterBottom>
+            Generate Pawcards
           </Typography>
           <TextField
             value={text}
@@ -174,17 +116,13 @@ export default function Generate() {
             sx={{ mb: 2 }}
           />
           <Button
-          color="inherit"
-          href="/generate"
-          sx={{
-            backgroundColor: '#B06500', // Dark orange
-            '&:hover': {
-              backgroundColor: '#E67E22' // Slightly lighter dark orange on hover
-            },
-            color: '#FFFFFF'
-          }}
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit}
+            fullWidth
+            sx={{ backgroundColor: '#CC5500', '&:hover': { backgroundColor: '#FF6E00' } }}
           >
-            Generate Flashcards
+            Generate Pawcards
           </Button>
         </Box>
         {flashcards.length > 0 && (
@@ -196,26 +134,39 @@ export default function Generate() {
               alignItems: "center",
             }}
           >
-            <Typography variant="h5" component="h2" gutterBottom
-            sx={{fontFamily: '"impact", "Arial Black", sans-serif',
-              color: '#fff', // White text
-              textAlign: 'center'}}>
-              Generated Pawcards
-            </Typography>
-            <Grid container spacing={2}>
+            <Grid container spacing={2} style={{
+        backgroundColor: '#E77D22', // Orange background color
+        padding: '16px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: '8px', // Rounded corners
+        height: '100%',
+        width: '100%',
+      }}>
               {flashcards.map((flashcard, index) => (
                 <Grid item xs={12} sm={6} md={4} key={index}>
-                  <ReactFlipCard
-                    containerStyle={{
-                      width: "100%",
-                      height: "200px",
-                      position: "relative",
-                    }}
-                    flipTrigger={"onClick"}
-                    frontComponent={<FlipCard>{flashcard.front}</FlipCard>}
-                    backComponent={<FlipCard>{flashcard.back}</FlipCard>}
-                  />
-                </Grid>
+                <ReactFlipCard
+                  containerStyle={{
+                    width: "100%",
+                    height: "200px",
+                    position: "relative",
+                    border: "4px solid #8A3324",
+                    borderRadius: "8px",
+                  }}
+                  flipTrigger={"onClick"}
+                  frontComponent={
+                    <FlipCard>
+                      {flashcard.front}
+                    </FlipCard>
+                  }
+                  backComponent={
+                    <FlipCard>
+                      {flashcard.back}
+                    </FlipCard>
+                  }
+                />
+              </Grid>
               ))}
             </Grid>
             <Box sx={{ mt: 4, display: "flex", justifyContent: "center" }}>
@@ -223,6 +174,7 @@ export default function Generate() {
                 variant="contained"
                 color="secondary"
                 onClick={handleOpenDialog}
+                sx={{ backgroundColor: '#CC5500', '&:hover': { backgroundColor: '#FF6E00' } }}
               >
                 Save
               </Button>
@@ -233,7 +185,7 @@ export default function Generate() {
           <DialogTitle>Save Pawcards</DialogTitle>
           <DialogContent>
             <DialogContentText>
-              Please enter a name for your flashcards collection.
+              Please enter a name for your Pawcards collection.
             </DialogContentText>
             <TextField
               autoFocus
